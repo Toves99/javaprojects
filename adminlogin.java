da45f6130ffd85ba748022560d26f57d6cc90dba
toves99 <toves900@gmail.com>
2022-09-30 14:00:44 -0700

    adminlogin added

---
+++ adminlogin.java
@@ -0,0 +1,22 @@
+package adminjavafxView;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class AdminLogin extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.initStyle(StageStyle.UNDECORATED);
+        Parent root= FXMLLoader.load(getClass().getResource("adminLogin.fxml"));
+        stage.setScene(new Scene(root,700,500));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
